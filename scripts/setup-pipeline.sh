7708793056a0e27b152838cb0d5a2e9e0224d34e
kitty-catt <kitty-catt@example.com>
2020-10-25 07:10:58 +0100

    preparations for build and push to quay

--- scripts/setup-pipeline.sh
+++ scripts/setup-pipeline.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 source config
-echo "setting up generic openshift pipeline"
+echo "Setting up generic openshift pipeline"
 
 oc apply -f ../tekton-pipelines/pipeline.yaml 
 oc apply -f ../tekton-tasks/appsody-build-push.yaml 
 
+oc create sa appsody-sa
+oc policy add-role-to-user admin system:serviceaccount:$NAMESPACE:appsody-sa
 
+oc create secret docker-registry quay-cred \
+    --docker-server=quay.io \
+    --docker-username=${QUAY_USER} \
+    --docker-password=${QUAY_PWD} \
+    --docker-email=${QUAY_EMAIL}
+
+oc secrets link appsody-sa quay-cred
+oc describe sa appsody-sa
