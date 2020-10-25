cc49a223e1c2fbbdd23ab778d3b7745bca13b8b3
ronald van de kuil <ronald@oc1512264167.ibm.com>
2020-10-25 11:23:56 +0100

    use a generated name

--- scripts/inventory/build.sh
+++ scripts/inventory/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "building inventory image"
-oc apply -f $HERE/tekton-pipeline-run/inventory-run.yaml
+#oc apply -f $HERE/tekton-pipeline-run/inventory-run.yaml
+oc create -f $HERE/tekton-pipeline-run/inventory-run-auto.yaml
 
+#Note: the inventory-run-auto.yaml will generate a name for the pipelinerun, it MUST use oc create/
