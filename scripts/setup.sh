0fed0e1fc16a0e8a817d706882622283aadca13f
ronald van de kuil <ronald@oc1512264167.ibm.com>
2020-10-25 08:38:33 +0100

    prompt rm

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,17 @@
-source config
+#!/bin/bash
+
+export HERE=${PWD}
+echo "Working from $HERE"
+
+source $HERE/config
 
 oc new-project $NAMESPACE
 
-echo "temporary artifacts are stored in: " ~/$WORKSPACE
+echo "temporary artifacts are stored in: " /tmp/$WORKSPACE
+exit
 
-rm -rf ~/$WORKSPACE
-mkdir -pv ~/$WORKSPACE
+rm -i /tmp/$WORKSPACE
+mkdir -pv /tmp/$WORKSPACE
 
 bash setup-pipeline.sh
 bash inventory/setup-inventory-ms.sh
