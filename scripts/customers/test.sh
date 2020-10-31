85afcf585841f43978246c9fce70bc0e92e87d1f
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-10-31 11:08:53 +0100

    keep jwt tokens in handy

--- scripts/customers/test.sh
+++ scripts/customers/test.sh
@@ -8,3 +8,9 @@ curl -w "\n" $ROUTE
 echo "login manually on http://$ROUTE/_utils/#login "
 
 echo "TODO: check customer service"
+
+# default JWT admin token:
+#eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJhZG1pbiJdLCJ1c2VyX25hbWUiOiJmb28ifQ.hZEmuywb637OrP_6AKDiyZ5_mZ1lVJlwzCOG4egLa1c
+
+# default JWT blue token:
+#eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJibHVlIl0sInVzZXJfbmFtZSI6IiJ9.PGVxVMaxtAL2kh5ik1H6tcKLDv0xeh7klrJvrxEny48
