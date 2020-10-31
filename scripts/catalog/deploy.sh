f2e196cfa877af82c0fa9e33da53302617038479
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-10-31 07:28:06 +0100

    happy deploy of catalog

--- scripts/catalog/deploy.sh
+++ scripts/catalog/deploy.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 echo "deploy catalog"
+
+# appsody run --docker-options "\
+# -e ELASTIC_CLUSTER_NAME=docker-cluster \
+# -e ELASTIC_NODE_URL=host.docker.internal:9300 \
+# -e INVENTORY_URL=http://docker.for.mac.localhost:8081/micro/inventory"
+
+#$ oc get svc
+#NAME                   TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)                      AGE
+#catalogelasticsearch   ClusterIP   172.21.175.126   <none>        9200/TCP,9300/TCP            16m
+#inventory              ClusterIP   172.21.49.223    <none>        8080/TCP,8443/TCP,8778/TCP   37m
+#inventorymysql         ClusterIP   172.21.245.138   <none>        3306/TCP                     68m
+
+oc new-app --name=catalog \
+   -e ELASTIC_CLUSTER_NAME=docker-cluster \
+   -e ELASTIC_NODE_URL=catalogelasticsearch:9300 \
+   -e INVENTORY_URL=http://inventory:8080/micro/inventory \
+   --image-stream=catalog \
+   --as-deployment-config
+
+oc expose svc/catalog
