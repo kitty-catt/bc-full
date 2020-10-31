e082ef61f34cb3ad983e84cf856b067f255202ba
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-10-31 10:56:45 +0100

    print admin URL for couchdb

--- scripts/customers/test.sh
+++ scripts/customers/test.sh
@@ -5,4 +5,6 @@ echo "testing customers deployment"
 export ROUTE=$(oc get route | grep customercouchdb | awk  '{ print $2}')
 curl -w "\n" $ROUTE
 
+echo "login manually on http://$ROUTE/_utils/#login "
+
 echo "TODO: check customer service"
