fa0da0e9fc80847f7685b8861fea484186cf0d71
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-10-31 11:24:07 +0100

    deploy customer service

--- scripts/customers/deploy.sh
+++ scripts/customers/deploy.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 echo "deploy customers"
+source ~/config
+
+#appsody run --docker-options "\
+# -e COUCHDB_PORT=5985 \
+# -e COUCHDB_HOST=host.docker.internal \
+# -e COUCHDB_PROTOCOL=http \
+# -e COUCHDB_USERNAME=admin \
+# -e COUCHDB_PASSWORD=passw0rd \
+# -e COUCHDB_DATABASE=customers 
+# -e HS256_KEY=E6526VJkKYhyTFRFMC0pTECpHcZ7TGcq8pKsVVgz9KtESVpheEO284qKzfzg8HpWNBPeHOxNGlyudUHi6i8tFQJXC8PiI48RUpMh23vPDLGD35pCM0417gf58z5xlmRNii56fwRCmIhhV7hDsm3KO2jRv4EBVz7HrYbzFeqI45CaStkMYNipzSm2duuer7zRdMjEKIdqsby0JfpQpykHmC5L6hxkX0BT7XWqztTr6xHCwqst26O0g8r7bXSYjp4a"
+
+oc new-app --name=customer \
+ -e COUCHDB_PORT=5984 \
+ -e COUCHDB_HOST=customercouchdb \
+ -e COUCHDB_PROTOCOL=http \
+ -e COUCHDB_USERNAME=${COUCHDB_USER} \
+ -e COUCHDB_PASSWORD=${COUCHDB_PASSWORD} \
+ -e COUCHDB_DATABASE=customers \
+ -e HS256_KEY=${HS256_KEY} \
+  --image-stream=customer \
+  --as-deployment-config
+
+oc expose svc/customer --port=8080
