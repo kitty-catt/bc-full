f6090e84b210d300733d445c1f99a37dbce9e53b
kitty-catt <kitty-catt@example.com>
2020-11-08 08:29:58 +0100

    deploy auth service

--- scripts/auth/deploy.sh
+++ scripts/auth/deploy.sh
@@ -2,3 +2,8 @@
 
 echo "deploy auth service for OAuth 2.0 authentication"
 
+oc new-app --name=auth \
+ -e HS256_KEY=${HS256_KEY} \
+  --image-stream=auth 
+
+oc expose svc/auth --port=8080
--- scripts/auth/setup.sh
+++ scripts/auth/setup.sh
@@ -2,4 +2,5 @@
 
 echo "setup auth service for OAuth 2.0 authentication"
 
+# no need to setup anything.
  
