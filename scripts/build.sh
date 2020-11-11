f974768a652c9c72856cfabb7e26cac12cf57ff1
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-11-11 15:24:13 +0100

    select build target

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,38 @@ echo "building images"
 export HERE=${PWD}
 echo "Working from $HERE"
 
-bash $HERE/scripts/catalog/build.sh
-bash $HERE/scripts/customers/build.sh
-bash $HERE/scripts/inventory/build.sh
-bash $HERE/scripts/orders/build.sh
-bash $HERE/scripts/web/build.sh
-bash $HERE/scripts/auth/build.sh
+echo "Select a build repository"
+echo
+
+# PS3 is an environment variable
+# that sets the menu prompt
+PS3="Choose a number: "
+
+# The select command creates the menu
+select CHOICE in quay ocp  Quit
+do
+    case $CHOICE in
+        "quay")
+            export TARGET=QUAY
+            break
+            ;;
+        "ocp")
+            export TARGET=OCP
+            break
+            ;;
+        "Quit")
+            exit
+            ;;
+        *)  echo "Invalid Choice";;
+    esac
+done    
+
+echo "TARGET=$TARGET"
+exit
+
+bash $HERE/scripts/catalog/build.sh $TARGET
+bash $HERE/scripts/customers/build.sh $TARGET
+bash $HERE/scripts/inventory/build.sh $TARGET
+bash $HERE/scripts/orders/build.sh $TARGET
+bash $HERE/scripts/web/build.sh $TARGET
+bash $HERE/scripts/auth/build.sh $TARGET
