8e61c833df43fe1e9947e83c03e4875c59d69ca1
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-11-12 12:18:00 +0100

    deploy orders

--- scripts/orders/deploy.sh
+++ scripts/orders/deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+source ~/config
 
 echo "deploy orders"
+
+# --as-deployment-config \
+oc new-app \
+ --name=orders \
+ ${OCNEWAPP_OPTION} \
+ --image-stream=orders \
+ -e jdbcURL=jdbc:mysql://ordersmysql:3307/${ORDER_DATABASE}?useSSL=true \
+ -e dbuser=${ORDER_USER} -e dbpassword=${ORDER_PASSWORD} \
+ -e jwksIssuer="https://localhost:9444/oidc/endpoint/OP"
+ 
+oc expose svc/orders
