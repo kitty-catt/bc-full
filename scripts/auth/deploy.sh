6b7fb117cf63daf5c0d019aba5b82b4f5eb59b66
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-11-18 11:11:31 +0100

    set customer-url

--- scripts/auth/deploy.sh
+++ scripts/auth/deploy.sh
@@ -4,7 +4,8 @@ source ~/config
 echo "deploy auth service for OAuth 2.0 authentication"
 
 oc new-app --name=auth-ms-spring \
- -e HS256_KEY=${HS256_KEY} \
+  -e HS256_KEY=${HS256_KEY} \
+  -e CUSTOMER_URL="http://customer-ms-spring:8080/micro/customer/search" \
   --image-stream=auth \
   -l app.kubernetes.io/part-of=auth-subsystem
 
--- scripts/customers/deploy.sh
+++ scripts/customers/deploy.sh
@@ -20,7 +20,6 @@ oc new-app --name=customer-ms-spring \
  -e COUCHDB_PASSWORD=${COUCHDB_PASSWORD} \
  -e COUCHDB_DATABASE=customers \
  -e HS256_KEY=${HS256_KEY} \
- -e CUSTOMER_URL="http://customer-ms-spring:8080/micro/customer/search"
   --image-stream=customer  \
   -l app.kubernetes.io/part-of=customer-subsystem
 
