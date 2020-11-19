5a38fe969d9801552eb8cb53590942558af1e9cc
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-11-19 08:24:56 +0100

    override production.json using configmap

--- scripts/web/deploy.sh
+++ scripts/web/deploy.sh
@@ -4,7 +4,19 @@ echo "deploy web frontend"
 
 export ROUTE=$(oc get route | grep auth-ms-spring | awk  '{ print $2}')
 
-echo "ROUTE=>$ROUTE<"
+#echo "ROUTE=>$ROUTE<"
+
+cp $HERE/scripts/web/production-input.json \
+   $HERE/scripts/web/production.json
+
+sed -i "s/auth-ms-spring:8080/$ROUTE/" $HERE/scripts/web/production.json
+
+oc create cm config \
+ --from-file=$HERE/scripts/web/production.json \
+ --from-file=$HERE/scripts/web/default.json \
+ --from-file=$HERE/scripts/web/checks
+
+rm $HERE/scripts/web/production.json
 
 oc new-app --name=web \
    -e AUTH_HOST=$ROUTE  \
@@ -14,6 +26,8 @@ oc new-app --name=web \
    -e PORT=3000 \
    --image-stream=web \
    -l app.kubernetes.io/part-of=web-subsystem
+
+oc set volume deployment/web --add --type=configmap --mount-path=/project/user-app/config/ --configmap-name=config --name=production-config
 
 oc patch svc web -p '{"spec": {"ports": [{"port": 3000,"targetPort": 3000,"name": "node"}],"type": "LoadBalancer"}}'
 
