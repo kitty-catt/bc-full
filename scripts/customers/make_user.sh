a55e742b9c574b85e2bf49c27ae7f19f5ee99834
ronald van de kuil <ronald@li-9c760200-6fa3-11e5-ab54-5065f32e4194.ibm.com>
2020-11-23 10:08:36 +0100

    make a user

--- scripts/customers/make_user.sh
+++ scripts/customers/make_user.sh
@@ -3,6 +3,15 @@
 echo "testing customers deployment"
 source ~/config
 
+read -p 'User to create: ' -e -i 'Yoda' username
+
+read -p 'Password: ' -e -i 'Do-Or-Do-Not-There-Is-No-Try' password
+
+echo "Creating user ${username} with password ${password}."
+
+#exit
+
+
 # OPEN THE DOOR
 oc expose svc customer-ms-spring
 
@@ -14,11 +23,11 @@ export jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJhZG1pbiJdLCJ1c2Vy
 export ROUTE=$(oc get route | grep customer | grep -v couchdb | awk  '{ print $2}')
 
 # create a customer using the admin token
-curl -s -v -X POST -i "http://$ROUTE/micro/customer/add" -H "Content-Type: application/json" -H "Authorization: Bearer ${jwt}" -d "{\"username\": \"${TEST_USER}\", \"password\": \"bar\", \"firstName\": \"foo\", \"lastName\": \"bar\", \"email\": \"foo@bar.com\"}"
+curl -s -v -X POST -i "http://$ROUTE/micro/customer/add" -H "Content-Type: application/json" -H "Authorization: Bearer ${jwt}" -d "{\"username\": \"${username}\", \"password\": \"${password}\", \"firstName\": \"${username}\", \"lastName\": \"bar\", \"email\": \"${username}@jedi-masters.org\"}"
 
 # search a customer using the admin token
 #curl -s -X GET "http://$ROUTE/micro/customer/search?username=${TEST_USER}" -H 'Content-type: application/json' -H "${jwt}" 
-curl -s -v -X GET "http://$ROUTE/micro/customer/search?username=${TEST_USER}" -H 'Content-type: application/json' -H "Authorization: Bearer ${jwt}" | jq .
+curl -s -v -X GET "http://$ROUTE/micro/customer/search?username=${username}" -H 'Content-type: application/json' -H "Authorization: Bearer ${jwt}" | jq .
 
 # CLOSE THE DOOR
 oc delete route customer-ms-spring
