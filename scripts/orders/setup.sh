a7af1d46948c4250728755b8adcc5bdf64b988c7
kitty-catt <ronald@oc7427525576.ibm.com>
2020-12-20 09:11:31 +0100

    version 10.2 is not present on CRC (too old), level up to latest.

--- scripts/orders/setup.sh
+++ scripts/orders/setup.sh
@@ -29,7 +29,7 @@ oc new-app \
 -p MYSQL_USER=${ORDER_USER} \
 -p MYSQL_PASSWORD=${ORDER_PASSWORD} \
 -p MYSQL_DATABASE=${ORDER_DATABASE} \
--p MARIADB_VERSION=10.2 \
+-p MARIADB_VERSION=latest \
 -p VOLUME_CAPACITY=1Gi \
   -l app.kubernetes.io/part-of=order-subsystem
 
