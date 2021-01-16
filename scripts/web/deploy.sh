28a3984519795001e9770c2d38d490f82016ff68
ronald van de kuil <ronald@oc3377678453.ibm.com>
2021-01-16 14:41:00 +0100

    can deploy garage main and old kitty-catt forks

--- scripts/web/deploy.sh
+++ scripts/web/deploy.sh
@@ -1,17 +1,54 @@
 #!/bin/bash
 
+source ~/config
+export HERE=${PWD}
+echo "Working from $HERE"
+
 echo "deploy web frontend"
 
-# Give it some time to manifest the route to the auth service
-sleep 10
-export ROUTE=$(oc get route | grep auth-ms-spring | awk  '{ print $2}')
-echo "ROUTE=>$ROUTE<"
-sleep 10
+echo "Select a repo type that you have build"
+echo
 
-cp $HERE/scripts/web/production-input.json \
-   $HERE/scripts/web/production.json
+# PS3 is an environment variable that sets the menu prompt
+PS3="Choose a number: "
 
-sed -i "s/auth-ms-spring:8080/$ROUTE/" $HERE/scripts/web/production.json
+# The select command creates the menu
+select CHOICE in ibm-garage old-kitty-catt-forks  Quit
+do
+    case $CHOICE in
+        "ibm-garage")
+            export REPO=ibm-garage
+
+            # move to new endpoints
+            cp $HERE/scripts/web/production-ocp.json \
+               $HERE/scripts/web/production.json
+            break
+            ;;
+        "old-kitty-catt-forks")
+            export REPO=kitty-catt
+
+            echo "searching for route to auth-ms-spring service"
+            # Give it some time to manifest the route to the auth service
+            sleep 10
+            export ROUTE=$(oc get route | grep auth-ms-spring | awk  '{ print $2}')
+            echo "ROUTE=>$ROUTE<"
+            sleep 10
+
+            cp $HERE/scripts/web/production-input.json \
+               $HERE/scripts/web/production.json
+
+            sed -i "s/auth-ms-spring:8080/$ROUTE/" $HERE/scripts/web/production.json            
+            break
+            ;;
+        "Quit")
+            exit
+            ;;
+        *)  echo "Invalid Choice";;
+    esac
+done    
+
+echo "REPO=$REPO"
+#exit
 
 oc delete cm config 2>/dev/null
 
@@ -31,7 +68,7 @@ oc new-app --name=web \
    --image-stream=web \
    -l app.kubernetes.io/part-of=web-subsystem
 
-oc set volume deployment/web --add --type=configmap --mount-path=/project/user-app/config/ --configmap-name=config --name=production-config
+oc set volume deployment/web --add --type=configmap --mount-path=/project/user-app/config/ --configmap-name=config --name=production-config  --overwrite
 
 oc patch svc web -p '{"spec": {"ports": [{"port": 3000,"targetPort": 3000,"name": "node"}],"type": "LoadBalancer"}}'
 
